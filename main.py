410433958d2301764d2c26a1ec37f8791b4fe0dc
Jonathan Li <jonathanlingjieli@gmail.com>
2019-10-13 00:35:12 -0700

    add backwards compatibility
    Signed-off-by: Jonathan Li <jonathanlingjieli@gmail.com>

--- main.py
+++ main.py
@@ -179,8 +179,8 @@ def main_worker(args):
 
     # create model
     model = models.__dict__[args.model]
-    if args.model != 'efficientnet':
-        model_config = {'dataset': args.dataset}
+    model_config = {'dataset': args.dataset} if args.model != 'efficientnet' \
+                                                              else dict()
 
     if args.model_config is not '':
         model_config = dict(model_config, **literal_eval(args.model_config))
