4db3c911cb503aa48eebc62217547527621f9ba3
Jonathan Li <jonathanlingjieli@gmail.com>
2019-10-28 19:55:42 +0000

    updating training script
    Signed-off-by: Jonathan Li <jonathanlingjieli@gmail.com>

--- b0_cifar10.sh
+++ b0_cifar10.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=effnet_cifar10_1
 #SBATCH --partition=V100
 #SBATCH --gres=gpu:1
-#SBATCH --time=4:00:00
+#SBATCH --time=5:00:00
 #SBATCH --output=/home/jlli/logs/effnet_cifar10.out
 
 module load cuda/10.1
@@ -12,10 +12,11 @@ module load cudnn/7.6.2
 PYTHON=/home/jlli/anaconda3/envs/ml/bin/python
 MAIN=/home/jlli/convNet.pytorch/main.py
 DATA=/home/jlli/datasets
-OUTPUT=/home/jlli/convNet.pytorch/cifar10_results
+# OUTPUT=/home/jlli/convNet.pytorch/cifar10_results
+OUTPUT=/home/jlli/logs
 SEED=1
 LR=0.1
-BATCH=128
+BATCH=256
 DECAY=5e-4
 
 $PYTHON $MAIN \
@@ -23,7 +24,7 @@ $PYTHON $MAIN \
 	--datasets-dir $DATA \
 	--dataset cifar10 \
 	--model efficientnet \
-	--model-config  '{"resolution": 32, "num_classes": 10, "use_cifar": True, "weight_decay": 5e-4}' \
+	--model-config  '{"resolution": 32, "num_classes": 10, "use_cifar": True, "weight_decay": 5e-4, "sparsity": 0.2}' \
 	--input-size 32 \
 	--dtype float \
 	--autoaugment \
