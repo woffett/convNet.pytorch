c5a03a157228e104cda0b69edd690c082fae3f32
avnermay <avnermay@gmail.com>
2020-01-17 08:03:50 +0000

    ml_env update

--- create_ml_env.sh
+++ create_ml_env.sh
@@ -1,4 +1,4 @@
 . /proj/anaconda3/etc/profile.d/conda.sh
 conda env create -f distill_env.sh
 conda activate ml
-conda install pytorch=1.0.0 torchvision cudatoolkit=9.0 -c pytorch
+conda install pytorch==1.1.0 torchvision==0.3.0 cudatoolkit=9.0 -c pytorch
