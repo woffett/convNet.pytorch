701a6bcde399d5444de976ad1a7455d080799dbb
Avner May <avnermay@cs.stanford.edu>
2020-01-17 00:05:51 -0800

    Update to cmd_creator to deal with nested string issue

--- cmd_creator.py
+++ cmd_creator.py
@@ -6,19 +6,20 @@ def get_cmdfile_path(filename):
 
 def cmds_1_16_20_train_base_models():
     filename = get_cmdfile_path('1_16_20_train_base_models_cmds')
-    cmd_format_str = ('qsub -V -b y -wd /proj/distill/wd /proj/distill/git/convNet.pytorch/ml_env.sh '
-                          '\\"python /proj/distill/git/convNet.pytorch/main.py '
+    cmd_format_str = ('qsub -V -b y -wd /proj/distill/wd -v '
+                          'cmd="python /proj/distill/git/convNet.pytorch/main.py '
                           '--results-dir {} --datasets-dir {} --dataset {} --save {} --model {} '
                           '--model-config {} --input-size {} --dtype {} --autoaugment --device cuda '
-                          '--epochs {} --batch-size {} --lr {} --weight-decay {} --print-freq 10 --seed {}\\"\n')   
+                          '--epochs {} --batch-size {} --lr {} --weight-decay {} --print-freq 10 --seed {}"'
+                          '/proj/distill/git/convNet.pytorch/ml_env.sh\n')
     results_dir = '/proj/distill/results'
     data_dir = '/proj/distill/data'
     dataset = 'cifar100'
     # training resnet18 and mobilenet
     run_names = ['resnet18_1_16_20', 'mobilenet_1_16_20']
     models = ['resnet','mobilenet']
-    model_configs = ['\'{"groups": [1,1,1,1], "depth": 18, "width": [64, 128, 256, 512]}\'',
-                    '\'{"num_classes": 100, "cifar": True}\'']
+    model_configs = ['\'{\\"groups\\": [1,1,1,1], \\"depth\\": 18, \\"width\\": [64, 128, 256, 512]}\'',
+                    '\'{\\"num_classes\\": 100, \\"cifar\\": True}\'']
     input_size = 32
     dtype = 'float'
     epochs = 170
@@ -31,7 +32,7 @@ def cmds_1_16_20_train_base_models():
         for i,model in enumerate(models):
             model_config = model_configs[i]
             run_name = run_names[i]
-            f.write(cmd_format_str.format(results_dir, data_dir, run_name, dataset, model, model_config,
+            f.write(cmd_format_str.format(results_dir, data_dir, dataset, run_name, model, model_config,
                                           input_size, dtype, epochs, batch_size, lr, weight_decay, seed))
 
     # These are the commands that Jonathan used.
