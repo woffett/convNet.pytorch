dbb78a87d4a9b7664573f93314f52fb5b32604cf
Avner May <avnermay@cs.stanford.edu>
2020-01-16 16:47:38 -0800

    NIT update in helpers

--- helpers.py
+++ helpers.py
@@ -12,11 +12,11 @@ def get_base_dir():
     if is_windows():
         username = ('Avner' if (hostname == 'Avner-X1Carbon')
                     else 'avnermay')
-        path = 'C:\\Users\\{}\\Babel_Files\\smallfry'.format(username)
+        path = 'C:\\Users\\{}\\Babel_Files\\distill'.format(username)
     # elif hostname == 'DN0a22a222.SUNet':
     # elif 'DN' in hostname and '.SUNet' in hostname:
     elif '.stanford.edu' in hostname:
-        path = '/dfs/scratch0/avnermay/smallfry'
+        path = '/dfs/scratch0/avnermay/distill'
     else:
-        path = '/proj/smallfry'
+        path = '/proj/distill'
     return path
