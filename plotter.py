f5b4d85990808909315a2eff47d384558e2c77d3
avnermay <avnermay@gmail.com>
2020-01-24 01:07:11 +0000

    Update to plotter.py for HP tuning

--- plotter.py
+++ plotter.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 def plot_distillation_tune_hp_results():
     path_regex = '/proj/distill/results/2020-01-22-tuneDistillHP/*/*results.json'
     all_results = clean_results(gather_results(path_regex))
+    var_info = ['distributed', [False]]
+    ylims = [68,72]
     lrs = [0.01, 0.03, 0.1, 0.3, 1] # 5
     # alphas = [0, 0.1, 0.5, 0.9, 1] # 5
     kldiv_temps = [0.1, 1, 10, 100] # 4
@@ -11,6 +13,7 @@ def plot_distillation_tune_hp_results():
     # losses = ['mse','kldiv'] # 2
 
     # PLOT KLDIV RESULTS
+    i=0
     for temp in kldiv_temps:
         info_per_line = {}
         for lr in lrs:
@@ -20,8 +23,12 @@ def plot_distillation_tune_hp_results():
                 'distill_loss':['kldiv'],
                 'temperature':[temp]
             }
-        plot_driver(all_results, None, info_per_line, 'alpha', 'best_prec1')
+        plt.figure(i)
+        i += 1
+        plot_driver(all_results, {}, info_per_line, 'alpha', 'best_prec1', var_info=var_info)
+        plt.ylim(ylims)
         plt.savefig('/proj/distill/results/2020-01-22-tuneDistillHP/figures/kldiv_temp,{}_tune_hp_results.pdf'.format(temp))
+        plt.close()
 
     # PLOT MSE RESULTS
     info_per_line = {}
@@ -31,8 +38,11 @@ def plot_distillation_tune_hp_results():
             'lr':[lr],
             'distill_loss':['mse']
         }
-    plot_driver(all_results, None, info_per_line, 'alpha', 'best_prec1')
+    plt.figure(i)
+    plot_driver(all_results, {}, info_per_line, 'alpha', 'best_prec1', var_info=var_info)
+    plt.ylim(ylims)
     plt.savefig('/proj/distill/results/2020-01-22-tuneDistillHP/figures/mse_tune_hp_results.pdf')
+    plt.close()
 
 if __name__ == '__main__':
     plot_distillation_tune_hp_results()
